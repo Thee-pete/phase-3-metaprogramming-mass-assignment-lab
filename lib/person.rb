7a82b25c95a8146c48aba7a63b13aeefcf997452
Peter Ndumia <ndegwandumia@gmail.com>
2022-08-26 17:59:51 +0300

    Complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
   # your code here
+  def initialize(args)
+    args.each_key do |key, value|
+      self.class.attr_accessor(key)
+      self.send("#{key}=", args[key])
+    end
+    
+  end
+
 end
